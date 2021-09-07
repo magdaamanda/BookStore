9781e9ad3cfaa5a4eafd1005bc7070d890690da8
Amanda <magda.amanda94@gmail.com>
2021-09-07 20:34:21 +0300

    second version

--- BookStore/src/main/java/syksy2021/BookStore/domain/BookRepository.java
+++ BookStore/src/main/java/syksy2021/BookStore/domain/BookRepository.java
@@ -2,6 +2,6 @@ package syksy2021.BookStore.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Book, String> {
+public interface BookRepository extends CrudRepository<Book, Long> {
 	
 }
